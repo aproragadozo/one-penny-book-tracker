905c54c78ffce39e6a2943b481b4d4f8f7984537
aproragadozo <aproragadozo@gmail.com>
2024-09-06 12:46:28 +0200

    improved typechecking with isinstance

--- scraper.py
+++ scraper.py
@@ -31,17 +31,18 @@ def get_price(url):
     #return min(float_prices)
     # the error seemed to be that min() was called on an empty item, but the xpath expression seems to work
     # so trying if returning the element directly works
-    if type(prices) is not list:
+    if isinstance(prices, list):
         return float(prices.strip()[1:])
-    float_prices = []
-    for price in prices:
-        try:
-            if float(price.strip()[1:]) != 0.0:
-                float_prices.append(float(price.strip()[1:]))
-        except:
-            continue
-    #return [float(price.strip()[1:]) for price in prices if price != ""]
-    return min(float_prices)
+    else:
+        float_prices = []
+        for price in prices:
+            try:
+                if float(price.strip()[1:]) != 0.0:
+                    float_prices.append(float(price.strip()[1:]))
+            except:
+                continue
+        #return [float(price.strip()[1:]) for price in prices if price != ""]
+        return min(float_prices)
 
 """ Session = sessionmaker(bind=engine)
 session = Session() """
