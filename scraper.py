4b3abb1e24f70e8ddfdd7e795b741ef3fe9aa0aa
aproragadozo <aproragadozo@gmail.com>
2024-09-06 18:28:55 +0200

    using css selector instead of xpath to get price

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import requests
 from sqlalchemy.orm import sessionmaker
 from bs4 import BeautifulSoup
 from lxml import etree as et
+import re
 import time
 import random
 import csv
@@ -13,6 +14,7 @@ import csv
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36", 'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'
 }
+# will need to actually get the product id only from whatever url users enter
 
 wish_list = ["https://www.amazon.co.uk/dp/0091958482",
 "https://www.amazon.co.uk/gp/product/0137058268",
@@ -23,9 +25,14 @@ def get_price(url):
     response = requests.get(url, headers=header)
     soup = BeautifulSoup(response.content, "html.parser")
     dom = et.HTML(str(soup))
+    from_price = soup.find("span", class_="a-size-base olp-link aod-popover-caret-link")
+    label = from_price["aria-label"]
+    price = re.search("£(.*)+", label)
+    return float(price.group().strip()[1:])
     # returns all prices that are indicated on page as "from"
     # (can't go with css selector because there's no tag, just a string value sandwiched between two tags)
-    prices = dom.xpath('//span[@class="olp-from"]/following::text()')
+    prices = dom.xpath('//span[@id="productTitle"]/text()')
+    return prices[0].strip()
     # strip the pound signs and convert prices to floats for comparison
     #float_prices = [float(price.strip()[1:]) for price in prices]
     #return min(float_prices)
@@ -47,5 +54,4 @@ def get_price(url):
 
 """ Session = sessionmaker(bind=engine)
 session = Session() """
-
 
